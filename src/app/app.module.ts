a5674ccc85bed464695d8c53b074ea98b675f639
Barataman0 <viniviusmarcus00@gmail.com>
2021-01-04 01:52:18 -0300

    Register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,34 @@
 
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import {ButtonModule} from 'primeng/button';
+import {DropdownModule} from 'primeng/dropdown';
+import {InputTextModule} from 'primeng/inputtext';
+import {InputNumberModule} from 'primeng/inputnumber';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ButtonModule,
+    DropdownModule,
+    InputTextModule,
+    InputNumberModule,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/register-page/register-page.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'register-page',
+  templateUrl: './register-page.component.html',
+  styleUrls: ['./register-page.component.css']
+})
+export class RegisterPageComponent implements OnInit {
+  name: string;
+  price: number; // preço
+  categories: string[]; // array de string
+  selectedCategorie: string | null; // opção selecionada
+
+  constructor() {
+    this.categories = [
+      "laticínios",
+      "bebidas",
+      "congelados"
+    ]
+    this.name = "";
+    this.price = 0;
+    this.selectedCategorie = null;
+
+
+   }
+
+  ngOnInit(): void {
+  }
+
+}
